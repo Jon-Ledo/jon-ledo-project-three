cbea21269588fed385aca7b7e006e74b0f0c2e58
Jon-Ledo <jonledo.code@gmail.com>
2021-05-19 00:30:29 -0400

    added p tag, extra style

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,7 @@ const Header = () => {
     <header className="home">
       <NavBar />
       <h1 id="home">Online Converters</h1>
+      <p>Pick what you need from the top</p>
     </header>
   );
 }
